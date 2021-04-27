0a3a2fcd3bf1bc3b40b16b9196a5d4e31e8fbce6
Nest <taketake221080559@gmail.com>
2021-04-27 17:35:06 +0000

    05 create login system

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+#class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #devise :database_authenticatable, :registerable,
+         #:recoverable, :rememberable, :validatable
+#end
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable #←confirmableを追加
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
-Rails.application.routes.draw do
-  resources :products
+#Rails.application.routes.draw do
+  #devise_for :users
+  #resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+  resources :products
 end
